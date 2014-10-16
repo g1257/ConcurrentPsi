28d277731c24806c0827e6bd165ba52b9b62c35b
g1257 <gz1@ornl.gov>
2014-10-16 16:14:14 -0400

    files added

---
+++ src/CriticalStorageImpl.h
@@ -0,0 +1,69 @@
+#ifndef CRITICALSTORAGEIMPL_H
+#define CRITICALSTORAGEIMPL_H
+#include <cassert>
+#include "CriticalReal.h"
+
+namespace ConcurrencyPsi {
+
+template<ParallelTypeEnum type, typename RealType>
+class CriticalStorageImpl {
+
+public:
+
+	typedef CriticalReal<type,RealType> CriticalRealType;
+
+	CriticalStorageImpl() {}
+
+	~CriticalStorageImpl()
+	{
+		for (SizeType i = 0; i < values_.size(); ++i) {
+			delete values_[i];
+			values_[i] = 0;
+		}
+	}
+
+	void push(RealType* v)
+	{
+		CriticalRealType* cr = new CriticalRealType(v);
+		values_.push_back(cr);
+	}
+
+	void prepare(SizeType nthreads)
+	{
+		for (SizeType i = 0; i < values_.size(); ++i)
+			values_[i]->prepare(nthreads);
+	}
+
+	RealType& value(SizeType i, SizeType threadNum)
+	{
+		assert(values_.size() > i);
+		return values_[i]->operator()(threadNum);
+	}
+
+	void sync()
+	{
+		for (SizeType i = 0; i < values_.size(); ++i)
+			values_[i]->sync();
+	}
+
+	template<typename MpiType>
+	void sync(MpiType& mpi)
+	{
+		for (SizeType i = 0; i < values_.size(); ++i)
+			values_[i]->sync(mpi);
+	}
+
+private:
+
+	CriticalStorageImpl(const CriticalStorageImpl&);
+
+	CriticalStorageImpl& operator=(const CriticalStorageImpl& other);
+
+	typename PsimagLite::Vector<CriticalRealType*>::Type values_;
+
+}; // class CriticalStorageImpl
+
+} //namespace ConcurrencyPsi
+
+#endif // CRITICALSTORAGEIMPL_H
+
