a2f77bd86f4da3e1488f96b237bf2b40c087f269
g1257 <gz1@ornl.gov>
2014-10-16 18:10:38 -0400

    test4 added: MPI within MPI

---
+++ examples/test4.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include "../src/Parallelizer.h"
+#include "../src/CriticalStorage.h"
+
+class KernelInner {
+
+public:
+
+	typedef double RealType;
+
+	KernelInner(SizeType totalInner)
+	    : totalInner_(totalInner),
+	      outerIndex_(0)
+	{}
+
+	template<typename SomeCriticalStorageType>
+	void operator()(SizeType index,
+	                SizeType threadNum,
+	                SomeCriticalStorageType& cs) const
+	{
+		int totalKernel=100;
+		int bigNumber = 10000;
+		for (int j = 0; j < totalKernel; ++j) {
+			for (int mm = 0; mm < bigNumber; mm++)
+				cs.value(0,threadNum) += (index + 1.0)*(outerIndex_+1.0)*1e-6;
+		}
+	}
+
+	SizeType size() const { return totalInner_; }
+
+	void setOuter(SizeType outerIndex)
+	{
+		outerIndex_ = outerIndex;
+	}
+
+private:
+
+	SizeType totalInner_;
+	SizeType outerIndex_;
+};
+
+class KernelOuter {
+
+	typedef ConcurrencyPsi::Parallelizer<ConcurrencyPsi::TYPE_MPI,
+	                                     KernelInner> ParallelizerInnerType;
+	typedef ParallelizerInnerType::CriticalStorageType InnerStorageType;
+
+public:
+
+	typedef double RealType;
+
+	KernelOuter(SizeType totalOuter, SizeType totalInner, SizeType nthreads)
+	    : totalOuter_(totalOuter),
+	      kernelInner_(totalInner),
+	      pInner_(kernelInner_,nthreads)
+	{}
+
+	template<typename SomeCriticalStorageType>
+	void operator()(SizeType index,
+	                SizeType threadNum,
+	                SomeCriticalStorageType& cs)
+	{
+		kernelInner_.setOuter(index);
+		InnerStorageType storageInner;
+		RealType tmp = 0;
+		storageInner.push(&tmp);
+		pInner_.launch(storageInner);
+		storageInner.sync();
+		cs.value(0, threadNum) += tmp;
+	}
+
+	SizeType size() const { return totalOuter_; }
+
+private:
+
+	SizeType totalOuter_;
+	KernelInner kernelInner_;
+	ParallelizerInnerType pInner_;
+};
+
+int main(int argc, char* argv[])
+{
+	typedef ConcurrencyPsi::Parallelizer<ConcurrencyPsi::TYPE_MPI,
+	                                     KernelOuter> ParallelizerOuterType;
+	typedef KernelOuter::RealType RealType;
+
+	if (argc < 5) {
+		std::cerr<<"USAGE "<<argv[0]<<" totalOuter totalInner mpiInner mpiOuter\n";
+		return 1;
+	}
+
+	SizeType totalOuter = atoi(argv[1]);
+	SizeType totalInner = atoi(argv[2]);
+	SizeType mpiInner = atoi(argv[3]);
+	SizeType mpiOuter = atoi(argv[4]);
+	std::cout<<"nested loop example \n";
+	KernelOuter kernelOuter(totalOuter, totalInner, mpiInner);
+	ParallelizerOuterType pOuter(kernelOuter,mpiOuter,&argc,&argv);
+	ParallelizerOuterType::CriticalStorageType cs;
+	RealType tmp = 0;
+	cs.push(&tmp);
+	pOuter.launch(cs);
+
+	cs.sync();
+
+	if (ParallelizerOuterType::canPrint())
+		std::cout<<argv[0]<<" Final Result is "<<tmp<<"\n";
+}
+
